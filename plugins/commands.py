5180dae1e8eab1c830f8ac30ebc638f06fa7d117
wagger1 <119261400+wagger1@users.noreply.github.com>
2022-12-31 15:09:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -64,7 +64,7 @@ async def start(client, message):
             ]
         ]
 
-        if message.command[1] != "subscribe":
+        i⭕f message.command[1] != "subscribe":
             try:
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
@@ -96,7 +96,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
+            parse_mode=enums.ParseMode.HTML⭕
         )
         return
     data = message.command[1]
