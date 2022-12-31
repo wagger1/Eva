7e022d142ea9e81306e9b104aeddda588b5376f4
wagger1 <119261400+wagger1@users.noreply.github.com>
2022-12-31 14:34:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -94,15 +94,6 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
-            ]
-            for file in files
-        ]
-    else:
-        btn = [
-            [
-                InlineKeyboardButton(
                     text=f"🎭{get_size(file.file_size)} 🔸 {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
