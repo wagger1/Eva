4eb76cf5eda9abc0c18c361b754a94823b8d5425
wagger1 <119261400+wagger1@users.noreply.github.com>
2023-01-09 23:21:15 +0530

    Update ping.py

--- plugins/helpers/ping.py
+++ plugins/helpers/ping.py
@@ -9,4 +9,4 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"<b>Pong!\n{time_taken_s:.3f} ms\n\🙃</b>")
+    await rm.edit(f"<b>ᴄᴜʀʀᴇɴᴛ ᴘɪɴɢ ɪꜱ\n{time_taken_s:.3f} 🏓</b>")
