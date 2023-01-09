c18235b18c4ff479f6c7ec51c535eea1b4bf4b57
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2023-01-09 22:56:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,6 +17,9 @@ API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
 
+# Command
+COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
+
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
