c7487c8e16b904770f093c18b4adbc293f817116
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2023-01-09 22:54:34 +0530

    Create ping.py

---
+++ plugins/helpers/ping.py
@@ -0,0 +1,12 @@
+import time
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"<b>Pong!\n{time_taken_s:.3f} ms\n\🙃</b>")
