09cd544fe6b0cb6e49d0b842dd0bf97c1246dbc9
wagger1 <119261400+wagger1@users.noreply.github.com>
2023-01-14 16:48:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>എന്നോട് പറഞ്ഞിട്ട് കാര്യം ഇല്ല.. 🥲 ഗ്രൂപ്പിൽ ചോദിക്ക് 😜 \nᴀꜱᴋ ɪɴ ᴍʏ ɢʀᴏᴜᴘ 🙃🧨</b>")
+    await message.reply_text("<b>എന്നോട് പറഞ്ഞിട്ട് കാര്യം ഇല്ല.. 🥲 ഗ്രൂപ്പിൽ ചോദിക്ക് 😜 \nᴀꜱᴋ ɪɴ ᴍʏ ɢʀᴏᴜᴘ 🙃\n🦋@CinemaXpressTM🦋</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
