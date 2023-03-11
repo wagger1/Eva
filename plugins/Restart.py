898d1444bae549198a87c9716f247e5f8030f7b5
wagger1 <119261400+wagger1@users.noreply.github.com>
2023-03-11 13:36:25 +0530

    Update Restart.py

--- plugins/Restart.py
+++ plugins/Restart.py
@@ -6,7 +6,7 @@ from info import ADMINS
     
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def stop_button(bot, message):
-    msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)       
+    msg = await bot.send_message(text="**Hᴇʏ 🦇⃝🍂ꜱᴀᴄʜᴜ℡ Aʟʟ Mʏ Pʀᴏᴄᴇꜱꜱ Aʀᴇ Sᴛᴏᴘᴘᴇᴅ ⚙️ Wᴀɪᴛ Fᴏʀ 𝟻 Sᴇᴄꜱ 🤷‍♀️**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
-    await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
+    await msg.edit("**⚙️ Rᴇꜱᴛᴀʀᴛᴇᴅ ‼️ Nᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ 🙃**")
     os.execl(sys.executable, sys.executable, *sys.argv)
